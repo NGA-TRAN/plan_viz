7e52b45ae1e2654d1dd056bdabe3f0886b779654
Nga Tran <hoabinhnga.tran@datadoghq.com>
2025-11-27 20:51:19 -0500

    docs: add advantages of using Excalidraw

--- src/cli.ts
+++ src/cli.ts
@@ -19,7 +19,7 @@ const program = new Command();
 program
   .name('plan-viz')
   .description('Convert Apache Data Fusion Physical Execution Plans to Excalidraw JSON')
-  .version('0.1.5');
+  .version('0.1.6');
 
 program
   .option('-i, --input <file>', 'Input file containing the execution plan')
